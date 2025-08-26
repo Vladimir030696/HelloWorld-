93fc26f2650b949e9e2c47024dc51b27b14a7e46
Vladimir030696 <vova.eremeev.96@inbox.ru>
2025-08-26 16:44:17 +0900

    дз

---
+++ script.js
@@ -0,0 +1,37 @@
+let age = 10;
+alert (age);
+let a = 20;
+alert (a);
+let iphone = (`Год выпуска первого iphone 2007`);
+alert(iphone); 
+let js = (`Имя создателя языка JavaScript Брендан Эйх`);
+alert(js);
+let c = 10, b = 2;
+alert(c + b), alert(c - b),alert(c / b);
+let  q = 2, s = 5;
+let result = q**s;
+alert(result);
+let g = 9, m = 2;
+alert(g % m);
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+let ige = Number(prompt(`Сколько вам лет ?`))
+alert(ige);
+const user = {
+  name: "Владимир",
+  age: 29,           
+  isAdmin:  true       
+};
+let name = String(prompt(`Какоу у вас имя?`))
+alert(name);
+
+
+
+
+
