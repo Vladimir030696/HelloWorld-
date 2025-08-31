417062c8b51ed1894471ad83330a8bc56af5b040
Vladimir030696 <vova.eremeev.96@inbox.ru>
2025-08-31 20:08:19 +0900

    дз

--- script.js
+++ script.js
@@ -1,38 +1,70 @@
-let age = 10;
-alert (age);
-age = 20;
-alert (age);
-let iphone = (`Год выпуска первого iphone 2007`);
-alert(iphone); 
-let js = (`Имя создателя языка JavaScript Брендан Эйх`);
-alert(js);
-let c =  Number(10); b = Number(2);
-alert(c + b); 
-alert(c - b);
-alert(c * b);
-alert(c / b);
-let  q = 2; s = 5;
-let result = q**s;
-alert(result);
-let g = 9, m = 2;
-alert(g % m);
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num ++;
-num --;
-alert(num);
-let ige = Number(prompt(`Сколько вам лет ?`))
-alert(ige);
-const user = {
-  name: "Владимир",
-  age: 29,           
-  isAdmin:  true       
-};
-let name = String(prompt(`Какое у вас имя?`, ))
-alert("Привет,"+ name);
+let age = prompt('пароль?');
+let message;
+if(age >= 14) {
+    message = 'Пароль введен верно';
+} else {
+    message = 'Пароль введен неправильно';
+}
+alert(message);
+let c = Number(prompt(`Введите число`));
+(c >= 0 && c <= 10 ) ?  console.log(`Верно`) : console.log(` Не верно`);
+let d = 150;
+let e = 75;
+( d >= 100 || e >= 100) ? console.log(`Верно`) : console.log(`не верно`);
+let a = '2';
+let b = '3';
+alert(Number(a)+ Number(b));
+let  monthNumber = String(prompt(`Видите месяц`));
+  switch (monthNumber) {
+    case `1`:
+        console.log(`зима`)
+  break;
+    case `2`:
+        console.log(`зима`)
+  break;
+    case `3`:
+        console.log(`весна`)
+  break;
+    case `4`:
+        console.log(`весна`)
+  break;
+    case `5`:
+        console.log(`весна`)
+  break;
+    case `6`:
+        console.log(`лето`)
+  break;
+    case `7`:
+        console.log(`лето`)
+  break;
+    case `8`:
+        console.log(`лето`)
+  break;
+    case `9`:
+        console.log(`осень`)
+  break;
+    case `10`:
+        console.log(`осень`)
+  break;
+    case `11`:
+        console.log(`осень`)
+  break;
+    case `12`:
+        console.log(`зима`)
+  break;
+  default:
+      
+        console.log(`что-то не так`)
+  break;
+  
+  
+  }
+ 
+
+
+
+
+
 
 
 
